e96b6848eafe78921eeb57685181652be4bd2237
Dawid Smulewicz <dawid@necodeo.com>
2025-03-12 21:47:55 +0100

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,21 @@ import tailwindcss from "@tailwindcss/vite";
 import node from "@astrojs/node";
 import react from "@astrojs/react";
 
+const isProduction = process.env.NODE_ENV === 'production';
+
+const extra = isProduction ? {
+  ssr: {
+    noExternal: true
+  }
+} : {};
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
+    ...extra,
   },
   adapter: node({
     mode: "standalone"
