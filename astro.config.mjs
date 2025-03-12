e4792da3ac8fd21507d9a6429bbde7ef99a3bceb
Dawid Smulewicz <dawid@necodeo.com>
2025-03-12 21:33:10 +0100

    fix astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,48 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
+import { defineConfig, envField } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import node from "@astrojs/node";
+import react from "@astrojs/react";
 
-import tailwindcss from '@tailwindcss/vite';
+const isProduction = process.env.NODE_ENV === 'production';
+
+const extra = isProduction ? {
+  vite: {
+    ssr: {
+      noExternal: true
+    }
+  }
+} : {};
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
+  output: "server",
+  integrations: [tailwind({
+    nesting: true,
+    applyBaseStyles: true
+  }), react()],
   adapter: node({
-    mode: 'standalone',
+    mode: "standalone"
   }),
-  vite: {
-    plugins: [tailwindcss()]
-  }
+  experimental: {
+    env: {
+      schema: {
+        ASTRO_GUILDMETRICS_BASE_URL: envField.string({
+          context: "server",
+          access: "secret",
+          optional: false,
+        }),
+        ASTRO_GUILDMETRICS_USER: envField.string({
+          context: "server",
+          access: "secret",
+          optional: true,
+        }),
+        ASTRO_GUILDMETRICS_PASS: envField.string({
+          context: "server",
+          access: "secret",
+          optional: true,
+        }),
+      }
+    }
+  },
+  ...extra
 });
