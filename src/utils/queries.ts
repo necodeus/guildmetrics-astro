056da6ee5b103fb4ee1c6835d2035e5b90e2a9a9
Dawid Smulewicz <dawid@necodeo.com>
2025-03-13 23:23:33 +0100

    types

--- src/utils/queries.ts
+++ src/utils/queries.ts
@@ -1,63 +1,38 @@
-export type OrganizationType = {
-  name: string;
-  handle: string;
-  numUsedSeats: number;
-  numTotalSeats: number;
-  createdAt: string;
-  expiresAt: string;
-}
-
-export type UserType = {
-  handle: string;
-  firstName: string;
-  lastName: string;
-  level: number;
-  xp: number;
-  xpForLevel: number;
-  xpTotalForLevel: number;
-  lessonsCompleted: number;
-  gems: number;
-  achievements: any[];
-  courses: any[];
-  profileImageURL: string;
-  userAchievements: any[];
-  userCourses: any[];
-};
-
-type Response = {
-  organizations: OrganizationType[];
-  users: UserType[];
-  achievements: any[];
-  courses: any[];
-};
-
 import { GUILDMETRICS_API_URL } from "astro:env/server";
+import type { HandlersIndexResponse, HandlersUserResponse } from "../myApi";
 
-const sortByXP = (users: UserType[] = []) => users.sort((a, b) => b.xp - a.xp);
+const sortByXP = (users: HandlersUserResponse[]) => {
+  return users.sort((a, b) => {
+    if (a.xp && b.xp) {
+      return b.xp - a.xp;
+    }
 
-export const getUsers = async (): Promise<Response> => {
+    return 0;
+  });
+};
+
+export const getUsers = async () => {
   try {
     const response = await fetch(`${GUILDMETRICS_API_URL}/index`);
     const {
-        organizations,
-        users,
-        achievements,
-        courses,
-    }: Response = await response.json();
-  
-    return {
       organizations,
-      users: sortByXP(users),
+      users,
       achievements,
       courses,
-    }
-    
+    }: HandlersIndexResponse = await response.json();
+
+    return {
+      organizations,
+      users: sortByXP(users || []),
+      achievements,
+      courses,
+    };
   } catch (error) {
     return {
       organizations: [],
       users: [],
       achievements: [],
       courses: [],
-    }
+    };
   }
 };
